7c2baad8cb52ce6549cbb80467cf470bc6093c95
Julian One <38872371+julian-one@users.noreply.github.com>
2024-07-29 23:57:45 -0600

    better

--- nvim/.config/nvim/lua/julian-one/set.lua
+++ nvim/.config/nvim/lua/julian-one/set.lua
@@ -1,28 +1,13 @@
--- [[ Setting options ]]
--- See `:help vim.opt`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
---
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
-
--- Enable 24-bit RGB color in the TUI.
 vim.opt.termguicolors = true
-
--- Reduce time waiting after key press for better responsiveness in certain plugins.
 vim.opt.updatetime = 50
 
--- Make line numbers default
 vim.opt.number = true
 vim.opt.relativenumber = true
 
--- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
 
 vim.opt.tabstop = 4 -- Number of spaces that a <Tab> in the file counts for.
@@ -31,47 +16,26 @@ vim.opt.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent.
 vim.opt.expandtab = true -- Convert tabs to spaces.
 vim.opt.smartindent = true -- Automatically indent new lines.
 
--- no wrap
 vim.opt.wrap = false
-
--- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
 vim.opt.clipboard = 'unnamedplus'
 
--- Enable break indent
 vim.opt.breakindent = true
 
--- Save undo history
 vim.opt.swapfile = false -- Disable swap file creation.
 vim.opt.backup = false -- Disable backup file creation.
 vim.opt.undodir = os.getenv 'HOME' .. '/.vim/undodir' -- Set the directory for undo files.
 vim.opt.undofile = true -- Enable persistent undo.
 
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-
--- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
-
--- Decrease update time
-vim.opt.updatetime = 250
-
--- Decrease mapped sequence wait time
--- Displays which-key popup sooner
-vim.opt.timeoutlen = 300
 
 -- Configure how new splits should be opened
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
 -- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
@@ -84,13 +48,4 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
--- New Stuff
-vim.opt.textwidth = 80
-
--- for cmp
-vim.opt.completeopt = 'menuone,noselect'
-vim.opt.pumheight = 10
-vim.opt.pumblend = 10
-
--- gutter stuff
 vim.opt.signcolumn = 'yes'
