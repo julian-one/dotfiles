f1625fe8efbe2836a668dd116d1d975d7dd8c1db
Julian Roberts <38872371+julian-one@users.noreply.github.com>
2025-02-18 22:24:37 -0700

    minimal (#1)

---
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,29 @@
+vim.keymap.set('n', '<leader>n', vim.cmd.Ex, { desc = "Open [N]etrw" })
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]], { desc = "[Y]ank to system clipboard" })
+
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { noremap = true, silent = true, desc = "[Esc]ape terminal mode" })
+
+local terminal_buf = nil
+vim.keymap.set("n", "<leader>tt", function()
+  if terminal_buf and vim.api.nvim_buf_is_valid(terminal_buf) then
+    -- Terminal buffer exists; toggle its visibility
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+      if vim.api.nvim_win_get_buf(win) == terminal_buf then
+        vim.api.nvim_win_hide(win)
+        return
+      end
+    end
+    -- Terminal buffer is hidden; show it in a new split
+    vim.cmd.vsplit()
+    vim.api.nvim_win_set_buf(0, terminal_buf)
+    vim.cmd.wincmd("J")
+    vim.api.nvim_win_set_height(0, 15)
+  else
+    -- Create a new terminal buffer
+    vim.cmd.vsplit()
+    vim.cmd.term()
+    vim.cmd.wincmd("J")
+    vim.api.nvim_win_set_height(0, 15)
+    terminal_buf = vim.api.nvim_get_current_buf()
+  end
+end, { desc = "[T]oggle [T]erminal" })
---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,22 @@
+vim.opt.wrap = false
+vim.opt.mouse = ""
+
+-- Search
+vim.opt.hlsearch = false
+
+-- Numbering
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Indentation
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- Completions
+vim.opt.completeopt = { 'menuone', 'noselect' }
+vim.opt.pumheight = 10
+vim.opt.pumblend = 10
+vim.opt.signcolumn = 'yes'
