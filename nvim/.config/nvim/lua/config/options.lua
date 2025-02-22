db6186f8446a6e1f4f253e6ff856fa28c217e499
Julian One <38872371+julian-one@users.noreply.github.com>
2025-02-22 12:59:36 -0700

    quality of life

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,8 +1,8 @@
-vim.opt.wrap = false
 vim.opt.mouse = ""
 
 -- Search
 vim.opt.hlsearch = false
+vim.opt.incsearch = true
 
 -- Numbering
 vim.opt.number = true
@@ -15,8 +15,24 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
+vim.opt.wrap = false
+
 -- Completions
 vim.opt.completeopt = { 'menuone', 'noselect' }
 vim.opt.pumheight = 10
 vim.opt.pumblend = 10
+
+vim.opt.scrolloff = 8
 vim.opt.signcolumn = 'yes'
+
+-- Spell
+vim.opt.spell = true
+vim.opt.spelllang = "en"
+vim.opt.spellfile = vim.fn.stdpath("config") .. "/spell/en.utf-8.add"
+
+
+-- Undo
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
