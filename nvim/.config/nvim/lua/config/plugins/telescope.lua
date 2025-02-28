bcc6f51904d986219714f19b5d1c3e1525de21fb
julian-one <julian.roberts.one@gmail.com>
2025-02-28 11:06:24 -0700

    colors

--- nvim/.config/nvim/lua/config/plugins/telescope.lua
+++ nvim/.config/nvim/lua/config/plugins/telescope.lua
@@ -4,7 +4,8 @@ return {
         tag = '0.1.8',
         dependencies = {
             'nvim-lua/plenary.nvim',
-            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+            { 'nvim-tree/nvim-web-devicons',              enabled = vim.g.have_nerd_font },
         },
         config = function()
             local telescope = require('telescope')
