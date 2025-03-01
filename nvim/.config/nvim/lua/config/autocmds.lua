5df259fcbac76074e528ab32a1306702bb2836a1
Julian One <38872371+julian-one@users.noreply.github.com>
2025-02-28 20:02:15 -0700

    fomatting

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,34 +1,31 @@
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 vim.api.nvim_create_autocmd("TermOpen", {
-  group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
-  callback = function()
-    vim.opt.number = false
-    vim.opt.relativenumber = false
-  end,
+	group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
+	callback = function()
+		vim.opt.number = false
+		vim.opt.relativenumber = false
+	end,
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
-  desc = "Set up LSP keymaps when LSP attaches",
-  group = vim.api.nvim_create_augroup("custom-lsp-attach", { clear = true }),
-  callback = function(args)
-    local bufnr = args.buf
+	desc = "Set up LSP keymaps when LSP attaches",
+	group = vim.api.nvim_create_augroup("custom-lsp-attach", { clear = true }),
+	callback = function(args)
+		local bufnr = args.buf
 
-    -- Define LSP keymaps with descriptions
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = bufnr, desc = "[G]oto [D]efinition" })
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer = bufnr, desc = "[G]oto [R]eferences" })
-    vim.keymap.set('n', 'gI', vim.lsp.buf.implementation, { buffer = bufnr, desc = "[G]oto [I]mplementation" })
-    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, { buffer = bufnr, desc = "Type [D]efinition" })
-    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { buffer = bufnr, desc = "[R]e[N]ame Symbol" })
-    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { buffer = bufnr, desc = "[C]ode [A]ction" })
-    vim.keymap.set('n', '<leader>f', function()
-      vim.lsp.buf.format { async = true }
-    end, { buffer = bufnr, desc = "[F]ormat Buffer" })
-  end,
+		-- Define LSP keymaps with descriptions
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr, desc = "[G]oto [D]definition" })
+		vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = bufnr, desc = "[G]oto [R]eferences" })
+		vim.keymap.set("n", "gI", vim.lsp.buf.implementation, { buffer = bufnr, desc = "[G]oto [I]mplementation" })
+		vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, { buffer = bufnr, desc = "Type [D]definition" })
+		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = bufnr, desc = "[R]e[N]ame Symbol" })
+		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = bufnr, desc = "[C]ode [A]ction" })
+	end,
 })
--- nvim/.config/nvim/lua/config/plugins/which-key.lua
+++ nvim/.config/nvim/lua/config/plugins/which-key.lua
@@ -1,24 +1,24 @@
 return {
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    icons = {},
-    opts = {
-      spec = {
-        { "<leader>g", name = "[G]it" },
-        { "<leader>s", name = "[S]earch" },
-        { "<leader>t", name = "[T]erminal" },
-        { "<leader>c", name = "[C]ode" },
-      },
-    },
-    keys = {
-      {
-        "<leader>?",
-        function()
-          require("which-key").show({ mode = "n", auto = true })
-        end,
-        desc = "Show Normal Mode Keymaps",
-      },
-    },
-  }
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		icons = {},
+		opts = {
+			spec = {
+				{ "<leader>g", name = "[G]it" },
+				{ "<leader>s", name = "[S]earch" },
+				{ "<leader>t", name = "[T]erminal" },
+				{ "<leader>c", name = "[C]ode" },
+			},
+		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ mode = "n", auto = true })
+				end,
+				desc = "Show Normal Mode Keymaps",
+			},
+		},
+	},
 }
