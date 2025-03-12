9ced80769230d5de153a01223f088a702ce9bdf0
Julian One <38872371+julian-one@users.noreply.github.com>
2025-03-12 12:08:54 -0600

    kickstart

--- nvim/.config/nvim/lua/config/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/config/plugins/lspconfig.lua
@@ -1,136 +1,165 @@
 return {
 	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			{
-				"folke/lazydev.nvim",
-				ft = "lua",
-				opts = {
-					library = {
-						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-					},
-				},
-			},
-			{
-				"hrsh7th/cmp-nvim-lsp",
+		"folke/lazydev.nvim",
+		ft = "lua",
+		opts = {
+			library = {
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
 			},
 		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{ "williamboman/mason.nvim", opts = {} },
+			"williamboman/mason-lspconfig.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+			{ "j-hui/fidget.nvim", opts = {} },
+			"hrsh7th/cmp-nvim-lsp",
+		},
 		config = function()
-			local lspconfig = require("lspconfig")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+				callback = function(event)
+					local map = function(keys, func, desc, mode)
+						mode = mode or "n"
+						vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+					end
 
-			-- Lua LSP (lua_ls)
-			lspconfig.lua_ls.setup({
-				cmd = { "/usr/local/bin/lua-language-server" },
-				capabilities = capabilities,
-				settings = {
-					Lua = {
-						runtime = { version = "LuaJIT" },
-						diagnostics = { globals = { "vim" } },
-						workspace = {
-							checkThirdParty = false,
-							library = {
-								vim.env.VIMRUNTIME,
-								"${3rd}/luv/library",
-							},
-						},
-						telemetry = { enable = false },
-					},
-				},
-			})
+					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+					map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+					map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+					map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+					map(
+						"<leader>ws",
+						require("telescope.builtin").lsp_dynamic_workspace_symbols,
+						"[W]orkspace [S]ymbols"
+					)
+					map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+					map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
+					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
-			-- Go LSP (gopls)
-			lspconfig.gopls.setup({
-				cmd = { "gopls" },
-				capabilities = capabilities,
-				filetypes = { "go", "gomod", "gowork" },
-				root_dir = lspconfig.util.root_pattern("go.mod", ".git"),
-				settings = {
-					gopls = {
-						directoryFilters = { "-vendor" },
-						analyses = {
-							unusedparams = true,
-							shadow = true,
-						},
-						staticcheck = true,
-						usePlaceholders = true,
-						completeUnimported = true,
-						deepCompletion = true,
-					},
-				},
-			})
+					---@param client vim.lsp.Client
+					---@param method vim.lsp.protocol.Method
+					---@param bufnr? integer some lsp support methods only in specific files
+					---@return boolean
+					local function client_supports_method(client, method, bufnr)
+						if vim.fn.has("nvim-0.11") == 1 then
+							return client:supports_method(method, bufnr)
+						else
+							return client.supports_method(method, { bufnr = bufnr })
+						end
+					end
 
-			-- Templ
-			lspconfig.templ.setup({
-				cmd = { "templ", "lsp" },
-				capabilities = capabilities,
-				filetypes = { "templ" }, -- Ensure this is set properly
-				root_dir = lspconfig.util.root_pattern("go.mod", ".git"),
-			})
+					local client = vim.lsp.get_client_by_id(event.data.client_id)
+					if
+						client
+						and client_supports_method(
+							client,
+							vim.lsp.protocol.Methods.textDocument_documentHighlight,
+							event.buf
+						)
+					then
+						local highlight_augroup =
+							vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
+						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+							buffer = event.buf,
+							group = highlight_augroup,
+							callback = vim.lsp.buf.document_highlight,
+						})
 
-			-- sqls
-			lspconfig.sqls.setup({
-				cmd = { "sqls", "-c", vim.fn.expand("~/.sqls.yml") },
-				capabilities = capabilities,
-				filetypes = { "sql", "spanner" },
-				root_dir = lspconfig.util.root_pattern(".git", ".sqls.yml"),
-				on_attach = function(client, _)
-					-- Disable formatting
-					client.server_capabilities.documentFormattingProvider = false
-					client.server_capabilities.documentRangeFormattingProvider = false
+						vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+							buffer = event.buf,
+							group = highlight_augroup,
+							callback = vim.lsp.buf.clear_references,
+						})
+
+						vim.api.nvim_create_autocmd("LspDetach", {
+							group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
+							callback = function(event2)
+								vim.lsp.buf.clear_references()
+								vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
+							end,
+						})
+					end
+
+					if
+						client
+						and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf)
+					then
+						map("<leader>th", function()
+							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+						end, "[T]oggle Inlay [H]ints")
+					end
 				end,
 			})
 
-			-- TypeScript & JavaScript LSP (ts_ls)
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-				filetypes = { "typescript", "typescriptreact", "typescript.tsx", "javascript", "javascriptreact" },
-				root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git"),
-			})
-
-			-- Svelte LSP (svelte)
-			lspconfig.svelte.setup({
-				cmd = { "/opt/homebrew/bin/svelteserver", "--stdio" },
-				capabilities = capabilities,
-				filetypes = { "svelte" },
-				root_dir = lspconfig.util.root_pattern("package.json", ".git"),
-			})
-
-			-- HTML LSP (html)
-			lspconfig.html.setup({
-				capabilities = capabilities,
-				filetypes = { "html", "templ" },
-				root_dir = lspconfig.util.root_pattern("package.json", ".git"),
-			})
-
-			-- CSS LSP (cssls)
-			lspconfig.cssls.setup({
-				capabilities = capabilities,
-				filetypes = { "css", "scss", "less" },
-				root_dir = lspconfig.util.root_pattern("package.json", ".git"),
-				settings = {
-					css = { validate = true },
-					scss = { validate = true },
-					less = { validate = true },
+			vim.diagnostic.config({
+				severity_sort = true,
+				float = { border = "rounded", source = "if_many" },
+				underline = { severity = vim.diagnostic.severity.ERROR },
+				signs = vim.g.have_nerd_font and {
+					text = {
+						[vim.diagnostic.severity.ERROR] = "󰅚 ",
+						[vim.diagnostic.severity.WARN] = "󰀪 ",
+						[vim.diagnostic.severity.INFO] = "󰋽 ",
+						[vim.diagnostic.severity.HINT] = "󰌶 ",
+					},
+				} or {},
+				virtual_text = {
+					source = "if_many",
+					spacing = 2,
+					format = function(diagnostic)
+						local diagnostic_message = {
+							[vim.diagnostic.severity.ERROR] = diagnostic.message,
+							[vim.diagnostic.severity.WARN] = diagnostic.message,
+							[vim.diagnostic.severity.INFO] = diagnostic.message,
+							[vim.diagnostic.severity.HINT] = diagnostic.message,
+						}
+						return diagnostic_message[diagnostic.severity]
+					end,
 				},
 			})
 
-			-- Tailwind
-			lspconfig.tailwindcss.setup({
-				cmd = { "tailwindcss-language-server", "--stdio" },
-				capabilities = capabilities,
-				filetypes = {
-					"templ",
-					"html",
-					"css",
-					"scss",
-					"javascript",
-					"javascriptreact",
-					"typescript",
-					"typescriptreact",
-					"svelte",
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+
+			local servers = {
+				gopls = {},
+				ts_ls = {},
+				tailwindcss = {},
+				templ = {},
+				svelte = {},
+				html = {},
+				cssls = {},
+				lua_ls = {
+					settings = {
+						Lua = {
+							completion = {
+								callSnippet = "Replace",
+							},
+						},
+					},
 				},
-				root_dir = lspconfig.util.root_pattern("tailwind.config.js"),
+			}
+
+			local ensure_installed = vim.tbl_keys(servers or {})
+			vim.list_extend(ensure_installed, {
+				"stylua",
+			})
+			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+
+			require("mason-lspconfig").setup({
+				ensure_installed = {},
+				automatic_installation = false,
+				handlers = {
+					function(server_name)
+						local server = servers[server_name] or {}
+						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+						require("lspconfig")[server_name].setup(server)
+					end,
+				},
 			})
 		end,
 	},
