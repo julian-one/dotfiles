d57155a3ddf2f235be323b25edde0de52553076a
Julian One <38872371+julian-one@users.noreply.github.com>
2025-03-19 18:58:42 -0600

    spell, scroll

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,7 +1,11 @@
 vim.opt.mouse = ""
 
+-- Appearance
 vim.g.have_nerd_font = false
 vim.opt.termguicolors = true
+vim.opt.wrap = false
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
 
 -- Search
 vim.opt.hlsearch = false
@@ -21,12 +25,12 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.breakindent = true
 
-vim.opt.wrap = false
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-
 -- Undo
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
+
+-- Spell
+vim.opt.spelllang = "en_us"
+vim.opt.spell = true
