2b2a90364ae8db955fa2a7bb3067213d8e566a77
Julian One <38872371+julian-one@users.noreply.github.com>
2025-03-25 02:08:48 -0600

    folds

--- nvim/.config/nvim/lua/config/plugins/which-key.lua
+++ nvim/.config/nvim/lua/config/plugins/which-key.lua
@@ -2,6 +2,10 @@ return {
 	{
 		"folke/which-key.nvim",
 		event = "VeryLazy",
+		dependencies = {
+			"echasnovski/mini.icons",
+			"nvim-tree/nvim-web-devicons",
+		},
 		opts = {
 			delay = 0,
 			icons = {
@@ -12,6 +16,7 @@ return {
 				{ "<leader>c", group = "[C]ode" },
 				{ "<leader>d", group = "[D]ocument" },
 				{ "<leader>r", group = "[R]ename" },
+				{ "<leader>o", group = "[O]pen" },
 				{ "<leader>s", group = "[S]earch" },
 				{ "<leader>t", group = "[T]oggle" },
 				{ "<leader>g", group = "[G]it" },
