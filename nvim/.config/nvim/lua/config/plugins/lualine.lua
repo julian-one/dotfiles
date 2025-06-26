6de231fbf3e339ff55c62676c8491aedb2928bf4
Julian One <38872371+julian-one@users.noreply.github.com>
2025-06-26 09:10:15 -0600

    search

--- nvim/.config/nvim/lua/config/plugins/lualine.lua
+++ nvim/.config/nvim/lua/config/plugins/lualine.lua
@@ -57,7 +57,13 @@ return {
 				lualine_z = {},
 			},
 			tabline = {},
-			winbar = {},
+			winbar = {
+				lualine_c = {
+					"navic",
+					color_correction = nil,
+					navic_opts = nil,
+				},
+			},
 			inactive_winbar = {},
 			extensions = {},
 		})
--- nvim/.config/nvim/lua/config/plugins/telescope.lua
+++ nvim/.config/nvim/lua/config/plugins/telescope.lua
@@ -1,12 +1,11 @@
 return {
 	{
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.8",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 			{ "nvim-telescope/telescope-ui-select.nvim" },
-			{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+			{ "nvim-tree/nvim-web-devicons" },
 		},
 		config = function()
 			local telescope = require("telescope")
