9a7a287cc9c18c2ca2f3fc781683ae7247c6b16c
Julian One <38872371+julian-one@users.noreply.github.com>
2025-06-26 00:24:15 -0600

    cleanup

--- nvim/.config/nvim/lua/config/plugins/blink.lua
+++ nvim/.config/nvim/lua/config/plugins/blink.lua
@@ -1,8 +1,43 @@
 return {
 	{
+		"onsails/lspkind.nvim",
+		opts = {
+			mode = "symbol_text", -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
+			preset = "default",
+			symbol_map = {
+				Text = "",
+				Method = "",
+				Function = "",
+				Constructor = "",
+				Field = "",
+				Variable = "",
+				Class = "",
+				Interface = "",
+				Module = "",
+				Property = "",
+				Unit = "",
+				Value = "",
+				Enum = "",
+				Keyword = "",
+				Snippet = "",
+				Color = "",
+				File = "",
+				Reference = "",
+				Folder = "",
+				EnumMember = "",
+				Constant = "",
+				Struct = "",
+				Event = "",
+				Operator = "",
+				TypeParameter = "",
+			},
+		},
+	},
+	{
 		"saghen/blink.cmp",
 		dependencies = {
 			"rafamadriz/friendly-snippets",
+			"nvim-tree/nvim-web-devicons",
 		},
 		version = "1.*",
 
@@ -13,10 +48,62 @@ return {
 			appearance = {
 				nerd_font_variant = "normal",
 			},
+			signature = {
+				window = {
+					border = "rounded",
+					scrollbar = false,
+					max_height = 15,
+					winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None",
+				},
+			},
 			completion = {
 				documentation = {
 					auto_show = true,
-					auto_show_delay_ms = 500,
+					auto_show_delay_ms = 250,
+					window = {
+						border = "rounded",
+						scrollbar = false,
+						max_height = 15,
+						winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None",
+					},
+				},
+				menu = {
+					border = "rounded",
+					draw = {
+						components = {
+							kind_icon = {
+								text = function(ctx)
+									local icon = ctx.kind_icon
+									if vim.tbl_contains({ "Path" }, ctx.source_name) then
+										local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+										if dev_icon then
+											icon = dev_icon
+										end
+									else
+										icon = require("lspkind").symbolic(ctx.kind, {
+											mode = "symbol_text",
+										})
+									end
+
+									return icon .. ctx.icon_gap
+								end,
+
+								-- Optionally, use the highlight groups from nvim-web-devicons
+								-- You can also add the same function for `kind.highlight` if you want to
+								-- keep the highlight groups in sync with the icons.
+								highlight = function(ctx)
+									local hl = ctx.kind_hl
+									if vim.tbl_contains({ "Path" }, ctx.source_name) then
+										local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+										if dev_icon then
+											hl = dev_hl
+										end
+									end
+									return hl
+								end,
+							},
+						},
+					},
 				},
 			},
 			sources = {
