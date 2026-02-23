c0f47a226c23b3c2a82900f1282b2081d4dba116
Julian One <38872371+julian-one@users.noreply.github.com>
2026-02-23 00:15:10 -0700

    no scripts for you

--- waybar/scripts/brightness.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-current=$(brightnessctl -m | cut -d',' -f4 | tr -d '%')
-input=$(rofi -dmenu -theme minimal -p "Brightness (1-100):" -mesg "Current: ${current}%")
-[[ "$input" =~ ^[0-9]+$ ]] && [[ "$input" -ge 1 ]] && [[ "$input" -le 100 ]] && brightnessctl set "${input}%"
--- waybar/scripts/uptime.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-uptime -p | sed 's/^up //;s/, / /g;s/ \(minute\|hour\|day\|week\)s\?/\L\1/g;s/minute/m/;s/hour/h/;s/day/d/;s/week/w/'
